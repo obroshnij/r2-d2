d4c79ce64c3b0b8a764314d8e890d1bddf95cdf8
Stanislav Tanko <stas.tanko@gmail.com>
2015-01-15 01:39:14 +0200

    Update unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,7 +8,7 @@ listen "/var/www/apps/r2-d2/socket/.unicorn.sock", :backlog => 64
 listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
-timeout 30
+timeout 3600
 
 # feel free to point this anywhere accessible on the filesystem
 pid "/var/www/apps/r2-d2/run/unicorn.pid"
