e795f3ed4e5dc7bc21f1aaab5cf2175f55f589ee
Stanislav Tanko <stas.tanko@gmail.com>
2016-03-14 19:33:14 +0200

    Adjust monthly reports for India

--- app/helpers/manager_tools_helper.rb
+++ app/helpers/manager_tools_helper.rb
@@ -49,7 +49,12 @@ module ManagerToolsHelper
     end
   end
   
+  def india?(employee)
+    employee.keys.include?("Bonus for evenings and nights (rupees)")
+  end
+  
   def generate_canned(employee, month, signature)
+    india?(employee)
     return "INSUFFICIENT DATA" if employee["To be paid"].nil? || employee["Working Shifts"].nil?
     
     canned = "Hello " + employee["Name"].split.first + ",\n\n"
@@ -61,7 +66,10 @@ module ManagerToolsHelper
     shifts << "You had " + employee["Working Shifts"].to_s + " working shifts in " + month
     shifts << employee["Sick"].to_s + " #{employee["Sick"] > 1 ? "days" : "day"} of sick leave" if employee["Sick"]
     shifts << employee["Unpaid"].to_s + " #{employee["Unpaid"] > 1 ? "shifts were" : "shift was"} taken as unpaid vacation" if employee["Unpaid"]
+    shifts << employee["Unpaid Vacations"].to_s + " #{employee["Unpaid Vacations"] > 1 ? "shifts were" : "shift was"} taken as unpaid vacation" if employee["Unpaid Vacations"]
     shifts << employee["Vacations"].to_s + " #{employee["Vacations"] > 1 ? "days" : "day"} of paid vacation" if employee["Vacations"]
+    shifts << employee["Paid Vacations"].to_s + " #{employee["Paid Vacations"] > 1 ? "days" : "day"} of paid vacation" if employee["Paid Vacations"]
+    
     if shifts.count > 1
       canned << shifts[0..-2].join(", ") + " and " + shifts.last + "."
     else
@@ -69,8 +77,9 @@ module ManagerToolsHelper
     end
     
     shifts = []
-    shifts << employee["Night Shifts"].to_s + " night #{"shift".pluralize(employee["Night Shifts"])} which will be paid at 1.5 rate" if employee["Night Shifts"] && employee["Night Shifts"].to_f != 0
+    shifts << employee["Night Shifts"].to_s + " night #{"shift".pluralize(employee["Night Shifts"])} which will be paid at 1.5 rate" if employee["Night Shifts"] && employee["Night Shifts"].to_f != 0 && !india?(employee)
     shifts << employee["Overtimes x 2"].to_s + " double-paid #{"overshift".pluralize(employee["Overtimes x 2"])}" if employee["Overtimes x 2"]
+    shifts << employee["Overtimes x 1"].to_s + " overtime #{"shift".pluralize(employee["Overtimes x 1"])} that will be paid additionally at x1 rate" if employee["Overtimes x 1"]
     shifts << employee["Shifts x 2"].to_s + " double-paid #{"shift".pluralize(employee["Shifts x 2"])}" if employee["Shifts x 2"]
     if shifts.count > 0
       shifts.first.split(" ").first.to_i > 1 ? canned << " There were " : canned << " There was "
@@ -102,6 +111,15 @@ module ManagerToolsHelper
     
     if employee["Additional Tasks/Comments"] && employee["Additional Tasks/Comments"].match(BONUS_FINE_REGEX)
       canned << "Also, you will get a #{employee["Additional Tasks/Comments"].match(BONUS_FINE_REGEX).to_s} bonus for ...\n\n"
+    end
+    
+    if india?(employee) && employee["Bonus for evenings and nights (rupees)"].to_i > 0
+      shifts = []
+      shifts << "#{employee["Night Shifts"]} night" if employee["Night Shifts"].to_i > 0
+      shifts << "#{employee["Night shifts"]} night" if employee["Night shifts"].to_i > 0
+      shifts << "#{employee["Evening Shifts"]} evening" if employee["Evening Shifts"].to_i > 0
+      shifts << "#{employee["Evening shifts"]} evening" if employee["Evening shifts"].to_i > 0
+      canned << "Additionally you will get #{employee["Bonus for evenings and nights (rupees)"].to_i} rupees for #{shifts.join(' and ')} shifts.\n\n"
     end
     
     canned << "Unfortunately, there will be a #{employee["Fines"].to_s.match(BONUS_FINE_REGEX).to_s} fine for ...\n\n" if employee["Fines"] && employee["Fines"].to_s.match(BONUS_FINE_REGEX)
