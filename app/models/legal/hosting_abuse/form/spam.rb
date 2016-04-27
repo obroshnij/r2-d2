d27ff3c3424604ef1682eca794d296393d45c662
Stanislav Tanko <stas.tanko@gmail.com>
2016-04-27 18:19:01 +0300

    Fix bug

--- app/models/legal/hosting_abuse/form/spam.rb
+++ app/models/legal/hosting_abuse/form/spam.rb
@@ -130,11 +130,11 @@ class Legal::HostingAbuse::Form::Spam
   end
   
   def reported_ip= ips
-    super ips.split.map(&:strip).join("\n")
+    super ips.split.map(&:strip).join("\n") if ips.present?
   end
   
   def blacklisted_ip= ips
-    super ips.split.map(&:strip).join("\n")
+    super ips.split.map(&:strip).join("\n") if ips.present?
   end
   
   def persist hosting_abuse
