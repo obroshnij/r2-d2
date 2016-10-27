e3d6c8876cf988fd93d37ad8efaaa8fa577e7191
Dmitry Pasechnik <dmitriy.pasechnik@zone3000.net>
2016-10-27 15:48:38 +0300

    fixed tiers enabled

--- app/models/domains/compensation/stats/count_by_tiers_enabled.rb
+++ app/models/domains/compensation/stats/count_by_tiers_enabled.rb
@@ -6,7 +6,7 @@ class Domains::Compensation::Stats::CountByTiersEnabled
 
   def data
     items = compensations
-    products.map { |product| count_cases(product, items) } + [count_total_cases(items)]
+    tier_pricings.map { |tier| count_cases(tier, items) } + [count_total_cases(items)]
   end
 
   private
@@ -23,11 +23,9 @@ class Domains::Compensation::Stats::CountByTiersEnabled
     Domains::Compensation::TierPricing.all
   end
 
-  def count_cases product, compensations
-    items = {pricings: []}
-    tier_pricings.each { |p| items[:pricings] << { name: p.name, count: compensations.where(product_id: product.id, tier_pricing_id: p.id).count } }
-    by_product = compensations.where(product_id: product.id)
-    { product_id: product.id, product: product.name, total: by_product.count }.merge items
+  def count_cases tier, compensations
+    by_tier = compensations.where(tier_pricing_id: tier.id)
+    { tier_id: tier.id, tier: tier.name, total: by_tier.count }
   end
 
   def count_total_cases(compensations)
