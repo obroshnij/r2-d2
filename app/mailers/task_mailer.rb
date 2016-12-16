387dadc1fd6244556049e2847da45725d2f8451d
Stanislav Tanko <stas.tanko@gmail.com>
2016-12-16 19:18:18 +0200

    update emails

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -3,7 +3,7 @@ class TaskMailer < ApplicationMailer
   def performance_issues_tracking(rows_need)
     @rows = rows_need
     mail(
-      to:      'stas.t@namecheap.com',
+      to:      ['nata@namecheap.com', 'richard.k@namecheap.com', 'sergey.s@namecheap.com'],
       subject: "Checkout and Website Performance Issues Tracking - #{Date.today.strftime('%d %b %Y')}"
     )
   end
