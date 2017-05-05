85740855eae0267a4b3550dbbb54f497b0b96b7f
Stanislav Tanko <stas.t@namecheap.com>
2017-05-05 13:17:53 +0300

    bug fix

--- lib/whois_parser.rb
+++ lib/whois_parser.rb
@@ -1,5 +1,5 @@
 module WhoisParser
-  
+
   def self.parse(domain_name, whois_record)
     properties = get_parser(domain_name).parse domain_name, whois_record
     if properties[:available] == 'unknown'
@@ -7,17 +7,17 @@ module WhoisParser
     end
     properties
   end
-  
+
   private
-  
+
   def self.get_tld(domain_name)
     PublicSuffix.parse(domain_name).tld
   end
-  
+
   def self.get_parser(domain_name)
-    const_get 'WhoisParser::' + get_tld(domain_name).split('.').last.upcase, false
+    "WhoisParser::#{get_tld(domain_name).split('.').last.upcase}".constantize
   rescue NameError
     WhoisParser::Base
   end
-  
+
 end
