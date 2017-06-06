bdc175d46d6bc9e82de5e1e843670832e5930cd7
Stanislav Tanko <stas.t@namecheap.com>
2017-06-06 14:50:16 +0300

    update emails

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -3,7 +3,7 @@ class TaskMailer < ApplicationMailer
   def performance_issues_tracking(rows_need)
     @rows = rows_need
     mail(
-      to:      ['nata@namecheap.com', 'richard.k@namecheap.com', 'sergey.s@namecheap.com', 'alina.b@namecheap.com'],
+      to:      ['nata@namecheap.com', 'richard.k@namecheap.com', 'sergey.s@namecheap.com', 'alina.b@namecheap.com', 'paul.r@namecheap.com', 'kate.sushkova@namecheap.com', 'anton.sheyko@namecheap.com', 'andrey.nesterenko@namecheap.com'],
       subject: "Checkout and Website Performance Issues Tracking - #{Date.today.strftime('%d %b %Y')}"
     )
   end
