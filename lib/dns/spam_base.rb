3f1db98f6c11e20f270ba33b1cb59f93c59a332a
Stanislav Tanko <stas.t@namecheap.com>
2017-06-29 18:52:03 +0300

    improve dns blacklist checkers

--- lib/dns/spam_base.rb
+++ lib/dns/spam_base.rb
@@ -13,25 +13,35 @@ module DNS
       "example.com"
     end
 
+    # def self.check_multiple domains
+    #   threads = [DNS::DBL.new, DNS::SURBL.new].map do |checker|
+    #     Thread.new(checker, domains) do |checker, domains|
+    #       domains.each { |domain| domain.extra_attr[checker.type] = checker.listed? domain.name }
+    #     end
+    #   end.each(&:join)
+    # end
+
     def self.check_multiple domains
-      threads = [DNS::DBL.new, DNS::SURBL.new].map do |checker|
-        Thread.new(checker, domains) do |checker, domains|
-          domains.each { |domain| domain.extra_attr[checker.type] = checker.listed? domain.name }
+      classes = [DNS::DBL, DNS::SURBL]
+      domains.map do |domain|
+        Thread.new(domain, classes) do |domain, classes|
+          checkers = classes.map(&:new)
+          checkers.each { |checker| domain.extra_attr[checker.type] = checker.listed?(domain.name) }
         end
       end.each(&:join)
     end
 
     def initialize
-      raise "Unable to initialize a #{type.to_s.upcase} checker" if nameservers.blank?
-      @checker = DNS::Resolver.new(type: :custom, nameservers: nameservers)
+      initialize_resolvers
     end
 
     def listed? domain
-      res = @checker.dig("#{domain}.#{self.class.base_host}", record: :a)
-      res.present?
+      get_a_record(domain).present?
     end
 
     private
+
+    attr_accessor :resolvers
 
     def nameservers
       @nameservers ||= get_nameservers
@@ -40,6 +50,30 @@ module DNS
     def get_nameservers
       path = File.join(File.dirname(__FILE__), "#{type}_ns.json")
       File.open(path, "r") { |f| JSON.parse f.read }
+    end
+
+    def initialize_resolvers
+      raise "Unable to initialize a #{type.to_s.upcase} checker" if nameservers.blank?
+      @resolvers = nameservers.map do |ns|
+        {
+          namserver: ns,
+          resolver:  DNS::Resolver.new(type: :custom, nameservers: ns)
+        }
+      end
+    end
+
+    def get_a_record domain
+      raise "Unable to get response from #{type.to_s.upcase} DNS servers, try again" if resolvers.blank?
+
+      ns = resolvers.sample
+      record = ns[:resolver].dig("#{domain}.#{self.class.base_host}", record: :a).first
+
+      if record == "No Response"
+        self.resolvers = resolvers.delete_if { |r| r[:nameserver] == ns[:nameserver] }
+        return get_a_record(domain)
+      end
+
+      record
     end
 
   end
