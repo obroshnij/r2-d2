82524ce383555ade3f79abd64c245fc57c056553
Stanislav Tanko <stas.t@namecheap.com>
2017-08-09 14:39:44 +0300

    update mailer

--- app/mailers/domain_watcher_mailer.rb
+++ app/mailers/domain_watcher_mailer.rb
@@ -1,8 +1,11 @@
 class DomainWatcherMailer < ApplicationMailer
-  
+
   def status_update(email, diff_val)
     @diff = diff_val
-    mail(to: email, subject: 'R2-D2: Domain Status Updates')
+    mail(
+      to: "#{email}, stas.t@namecheap.com",
+      subject: 'R2-D2: Domain Status Updates'
+    )
   end
-  
+
 end
