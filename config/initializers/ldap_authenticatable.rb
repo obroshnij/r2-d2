ddf811882c257317f70aa11c8a8e9543c102bbef
Stanislav Tanko <stas.t@namecheap.com>
2017-09-05 16:16:52 +0300

    ldap auth

--- config/initializers/ldap_authenticatable.rb
+++ config/initializers/ldap_authenticatable.rb
@@ -10,41 +10,41 @@ module Devise
       end
 
       def authenticate!
-        # if params[:user]
-        #   entries = ldap.bind_as(
-        #     base:     Rails.application.secrets.ldap_search_base,
-        #     filter:   "(samaccountname=#{uid})",
-        #     password: password
-        #   ) if uid.present?
-        #
-        #   if entries
-        #     user = User.from_ldap_entry entries.first
-        #     success! user
-        #   else
-        #     fail! 'Invalid user ID or password'
-        #   end
-        # end
         if params[:user]
-          uri = URI Rails.application.secrets.core_auth_url
-          req = Net::HTTP::Get.new(uri, 'Content-Type' => 'application/json')
-          payload = {
-            api_key:  Rails.application.secrets.core_api_key,
-            email:    uid,
+          entries = ldap.bind_as(
+            base:     Rails.application.secrets.ldap_search_base,
+            filter:   "(samaccountname=#{uid})",
             password: password
-          }
-          req.body = payload.to_json
-          res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-            http.request(req)
-          end
-          res = JSON.parse res.body
+          ) if uid.present?
 
-          if res["error"]
-            fail! 'Invalid user ID or password'
+          if entries
+            user = User.from_ldap_entry entries.first
+            success! user
           else
-            user = User.find_by_email res["namecheap_email"]
-            user ? success!(user) : fail!('Invalid user ID or password')
+            fail! 'Invalid user ID or password'
           end
         end
+        # if params[:user]
+        #   uri = URI Rails.application.secrets.core_auth_url
+        #   req = Net::HTTP::Get.new(uri, 'Content-Type' => 'application/json')
+        #   payload = {
+        #     api_key:  Rails.application.secrets.core_api_key,
+        #     email:    uid,
+        #     password: password
+        #   }
+        #   req.body = payload.to_json
+        #   res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        #     http.request(req)
+        #   end
+        #   res = JSON.parse res.body
+        #
+        #   if res["error"]
+        #     fail! 'Invalid user ID or password'
+        #   else
+        #     user = User.find_by_email res["namecheap_email"]
+        #     user ? success!(user) : fail!('Invalid user ID or password')
+        #   end
+        # end
       end
 
       def ldap
